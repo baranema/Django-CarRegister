caf8a6e03d8980f71ce805122948db3104a1fa76
baranema <Emukas_223>
2022-05-25 14:03:02 +0300

    added Car model to models.py and CarApp to the installed apps

--- CarRegister/CarApp/models.py
+++ CarRegister/CarApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+ carPlate = models.CharField(max_length=120)
+ carModel = models.CharField(max_length=120, help_text='Enter your full car model name.')
+ ownerfName = models.CharField(max_length=120)
+ ownerlName = models.CharField(max_length=120)
+ image = models.ImageField(upload_to= 'CarApp/', null=True) 
+ 
+ def __repr__(self):
+        return 'Car(%s, %s)' % (self.carPlate, self.carModel, self.ownerfName, self.ownerlName, self.image)
+
+def __str__ (self):
+    return self.title 
