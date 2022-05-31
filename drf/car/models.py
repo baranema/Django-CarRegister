2f97b0ff2dbb62c32dc4911f9fed5576c3ef96b2
baranema <Emukas_223>
2022-05-31 15:13:08 +0300

    added admin

--- drf/car/models.py
+++ drf/car/models.py
@@ -1,10 +1,10 @@
 from django.db import models
   
 class Car(models.Model):
+    #todo add validations for model/plate/firstname/lastname
     carPlateNumber = models.CharField(max_length=6, blank=False, unique=True,)
     carModel = models.CharField(max_length=200, blank=False, unique=False)
     ownerFirstName = models.CharField(max_length=200, blank=False, unique=False)
     ownerLastName = models.CharField(max_length=200, blank=False, unique=False)
     image = models.ImageField(upload_to='media', blank=True, unique=False, editable=False)
-    created_at = models.DateTimeField(auto_now=True)    
- 
+    created_at = models.DateTimeField(auto_now=True)
